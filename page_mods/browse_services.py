a314dab7eaa38e06000c2b71334171e7bfff75fa
visc0des <liethanseth@rogers.com>
2024-02-08 07:06:14 -0500

    Implemented private side of browsing and viewing (also cooldude.jpeg is also in the temp folder lmfao)

--- page_mods/browse_services.py
+++ page_mods/browse_services.py
@@ -45,6 +45,29 @@ def pull_public_files():
     return ret_html;
 
 
+def pull_private_files():
+    """ This function pulls the private files stored in the priv_files.pickle file. """
+
+    # Create temporary solution of returning a large string with the uploaded file info 
+    ret_html = "";
+
+    # Open the pickled file for public files and populate our python dict
+    PRIV_FILES = {};
+    try: 
+        priv_pickle = open('html/priv_files.pickle', 'rb');
+        PRIV_FILES = pickle.loads(priv_pickle.read());
+        priv_pickle.close();
+    except Exception as e:
+        print(f"\nCouldn't open pickle path. Likely doesn't exist. {e}")
+
+    # Iterate through pulled public files and return links to browsePublic.html for them to press and view the file in question. 
+    for file_name, file_data in PRIV_FILES.items():
+        ret_html += f"<a href='viewPrivate.html?name={file_name}'>{file_name}</a><br>"
+        
+    # Return the html
+    return ret_html;
+
+
 # --- MAIN for testing ---
 if __name__ == "__main__":
     pass;
--- page_mods/view_services.py
+++ page_mods/view_services.py
@@ -38,7 +38,7 @@ def pull_public_data(QUERY_VARS):
 
     # Check if file exists. Print error accordingly
     if requested_name not in PUB_FILES.keys():
-        return "<h2>ERROR: Requested file does not exist.</h3>"
+        return "<h2>ERROR: Requested file does not exist.</h3>";
     
     # Find the requested file by its name in public files, extract its data
     requested_file = PUB_FILES[requested_name];
@@ -50,8 +50,58 @@ def pull_public_data(QUERY_VARS):
 
     # Pull the requested file from database, save it temporarily,  
     file_ext = requested_file.name.split('.')[-1];
-    temp_name = f"downloaded-{requested_file.name}"
-    temp_path = f"./html/temp_folder/{temp_name}"
+    temp_name = f"downloaded-{requested_file.name}";
+    temp_path = f"./html/temp_folder/{temp_name}";
+    with open(temp_path, 'wb') as temp_file:
+        temp_file.write(requested_file.file_data);
+    
+    # If file was an image, also embed an image link to it in html. 
+    if file_ext == 'jpeg' or file_ext == 'jpg':
+        ret_html += f'<img src="./temp_folder/{temp_name}" alt="Requested Image" style="width:500px;height:300px">'
+
+    # Include a link to download it in the html.
+    ret_html += f'<br><br><a href="./temp_folder/{temp_name}" download>Download File</a>'
+
+    # Return the made html
+    return ret_html;
+
+
+def pull_private_data(QUERY_VARS):
+    """ Function returns in HTML the requested file information through the POST_DATA.
+    
+    "I'm making a private version of this to make development for future labs more convenient. Also because
+    it gives us a way to know to pull from the priv_files.pickle file. We could probably do some refactoring.
+
+    """
+
+    # Pull the name from the query 
+    requested_name = QUERY_VARS['name'][0];
+
+    # Open the pickled file for public files and populate our python dict
+    PRIV_FILES = {};
+    try: 
+        priv_pickle = open('html/priv_files.pickle', 'rb');
+        PRIV_FILES = pickle.loads(priv_pickle.read());
+        priv_pickle.close();
+    except Exception as e:
+        print(f"\nCouldn't open pickle path. Likely doesn't exist. {e}")
+
+    # Check if file exists. Print error accordingly
+    if requested_name not in PRIV_FILES.keys():
+        return "<h2>ERROR: Requested file does not exist.</h3>";
+    
+    # Find the requested file by its name in public files, extract its data
+    requested_file = PRIV_FILES[requested_name];
+    ret_html = f"""
+    <p><b>File name</b>: {requested_file.name} </p>
+    <p><b>Comment</b>: "{requested_file.comment}" </p>
+    <p><b>Time Uploaded</b>: {time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(requested_file.time_uploaded))} </p>
+    """
+
+    # Pull the requested file from database, save it temporarily,  
+    file_ext = requested_file.name.split('.')[-1];
+    temp_name = f"downloaded-{requested_file.name}";
+    temp_path = f"./html/temp_folder/{temp_name}";
     with open(temp_path, 'wb') as temp_file:
         temp_file.write(requested_file.file_data);
     
@@ -67,13 +117,14 @@ def pull_public_data(QUERY_VARS):
 
 
 
-def delete_image():
-    """ Function deletes the generated image sent with the request locally. Under a fixed name. """
+def flush_temp_folder():
+    """ This function flushes out the temp_folder of all files when called. Is called in every page."""
 
-    # NOTE WE NEED TO KEEP THE IMAGE OR TEXT FILE THERE WHILE THE PAGE IS STILL OPENED. ONCE THE PAGE EXITS (I.E. MOVE
-    # TO DIFFERENT PAGE, DOWNLOAD THE IMAGE ONCE, ETC.) uhh how do we deal with when the browser is closed? Give them a 
-    # time? 
+    # NOTE This is a good solution when there is a small amount of pages in the web application, i.e. in a dynamic 
+    # website. This becomes a concern however once the number of pages scale up, because that means this function
+    # has to included in each new webpage created, introducing another hassle. I can't seem to find a way to call it 
+    # automatically without having to modify demowww.py. 
+    # NOTE OOP, THIS DOESN'T GET CALLED WHEN THE 'GO BACK PAGE' BUTTON IS PRESSED. NEED A BETTER SOLUTION. 
 
-    # Have a temporary folder that can be cleaned out from the main page? 
-
-    pass;
+    for this_file in os.listdir('./html/temp_folder'):
+        os.remove('./html/temp_folder/' + str(this_file));
